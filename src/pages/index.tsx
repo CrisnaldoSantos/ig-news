e187050840f7d5b41626b4f4036ffac864b05bfe
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-02-05 11:37:22 -0300

    feat: include sevice stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import { SubscribeButton } from 'components/SubscribeButton'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { stripe } from 'services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,7 +26,7 @@ export default function Home() {
 
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -27,4 +35,25 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KPhiaFxOpZ2nFRdbLIPZAj8', {
+    expand: ['product'],
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('es-Us', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24horas
+  }
 }
