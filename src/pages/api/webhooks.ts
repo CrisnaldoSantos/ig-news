d0cb3e277cd67bba3b60b79d0b35b23e40964858
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-02-09 15:26:43 -0300

    feat: completed stripe webhooks update database

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { stripe } from 'services/stripe'
 import { Readable } from 'stream'
 import Stripe from 'stripe'
+import { saveSubscription } from './_lib/manageSubscription'
 
 async function buffer(readable: Readable) {
   const chunks = []
@@ -19,7 +20,11 @@ export const config = {
   },
 }
 
-const relevantsEvents = new Set(['checkout.session.completed'])
+const relevantsEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+])
 
 export default async function handler(
   req: NextApiRequest,
@@ -45,7 +50,23 @@ export default async function handler(
     if (relevantsEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+            break
           case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            )
             break
           default:
             throw new Error('Unhandled event.')
